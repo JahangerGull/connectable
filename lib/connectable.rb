f3148049fde7be1501c50960a36b892cc948810c
Jhanger Gul <jahanger.gull@gmail.com>
2012-07-20 15:11:26 +0500

    Fllowes list added

--- lib/connectable.rb
+++ lib/connectable.rb
@@ -6,17 +6,16 @@ module Connectable
     puts "Hello"
   end
 
-
   def followers(candidate_to_follow )
     Connectable.redis.hset(self, "User", candidate_to_follow)
     Connectable.redis.hset(candidate_to_follow, "User", self)
   end
 
-  def followers
-
+  def followers_list
+    Connectable.redis.get self
   end
 
-  def following
+  def following_list
 
   end
 
